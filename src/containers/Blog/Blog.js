eeab7f9df5e9fa3cd926750aaf6716178140ab87
Erik Forsström <erikforsstrom.kode@gmail.com>
2018-02-25 23:10:42 +0100

    last part of the routing module. Just before the assignment.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,27 +1,52 @@
 import React, { Component } from 'react';
 //import axios from 'axios';
-import { Route } from 'react-router-dom';
+import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
+//import NewPost from './NewPost/NewPost';
+import asyncComponent from '../../hoc/asyncComponent';
+
+const AsyncNewPost = asyncComponent(() => {
+  return import('./NewPost/NewPost');
+});
 
 class Blog extends Component {
+  state = {
+    auth: true
+  }
+
   render () {
     return (
       <div className="Blog">
         <header>
           <nav>
             <ul>
-              <li><a href="/">Home</a></li>
-              <li><a href="/new-post">New Post</a></li>
+              <li><NavLink
+                to="/posts/"
+                exact
+                activeClassName="active"
+                activeStyle={{
+                  color: '#fa923f',
+                  textDecoration: 'underline'
+                }}
+                  >Posts</NavLink></li>
+              <li><NavLink to={{
+                pathname: '/new-post',
+                hash: '#submit',
+                search: '?quick-submit=true'
+              }} exact>New Post</NavLink></li>
             </ul>
           </nav>
         </header>
         {/* <Route path="/" exact render={() => <h1>Home</h1>} />
         <Route path="/" exact render={() => <h1>Home 2</h1>} /> */}
-        <Route path="/" exact component={Posts} />
-        <Route path="/new-post" exact component={NewPost} />
+        <Switch>
+          {this.state.auth ? <Route path="/new-post" component={AsyncNewPost} /> : null}
+          <Route path="/posts" component={Posts} />
+          <Route render={() => <h1>Not Found</h1>} />
+          {/* <Redirect from="/" to="/posts" /> */}
+        </Switch>
       </div>
     );
   }
